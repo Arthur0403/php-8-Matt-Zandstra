fea083827afbada08c4097fd71e56b21cd265a8b
Arthur Lobanov <alobanov1989@yandex.ru>
2024-07-30 07:58:30 +0300

    Add listing 3-1, 3-4

---
+++ 03/ShopProduct.php
@@ -0,0 +1,10 @@
+<?php
+
+// Listing 3.1, 3.4
+class ShopProduct
+{
+    public $title = "Стандартный товар";
+    public $producerMainName = "Фамилия автора";
+    public $producerFirstName = "Имя автора";
+    public $price = 0;
+}
---
+++ 03/index.php
@@ -0,0 +1,7 @@
+<?php
+
+include_once 'ShopProduct.php';
+
+//3.2
+$product1 = new ShopProduct();
+$product2 = new ShopProduct();
