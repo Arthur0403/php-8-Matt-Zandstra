3425869c5f3c1c296b1ab097a617878168ad9710
Arthur Lobanov <alobanov1989@yandex.ru>
2024-07-31 08:47:28 +0300

    Add listing 3.29 используем контроль типа для write. Но, объявление типов классов проверяется во время выполнения программы.

--- 03/ShopProductWriter.php
+++ 03/ShopProductWriter.php
@@ -3,7 +3,7 @@
 // Listing 3.29
 class ShopProductWriter
 {
-    public function write($shopProduct)
+    public function write(ShopProduct $shopProduct)
     {
         $str = $shopProduct->title . ": " . $shopProduct->getProducer() . " (" . $shopProduct->price . ")\n";
         print $str;
@@ -14,3 +14,9 @@ class ShopProductWriter
 $product1 = new ShopProduct("Собачье сердце", "Михаил", "Булгаков", 5.99);
 $writer = new ShopProductWriter();
 $writer->write($product1);
+
+/*
+Принцип разделения ответственности.
+Класс ShopProduct - отвечает за хранение данных,
+а ShopProductWriter - за вывод этих данных.
+ */
