13f2bd1ecf47bba68df3cd714d4695ca6f27c58c
Arthur Lobanov <alobanov1989@yandex.ru>
2024-08-01 08:06:29 +0300

    Add listing 3.33 изменив таким образом метод-конструктор, можно гарантировать, что аргументы всегда будут содержать данные определенного типа

--- 03/ShopProduct.php
+++ 03/ShopProduct.php
@@ -1,15 +1,23 @@
 <?php
 
-// Listing 3.1, 3.4
+// Listing 3.1, 3.4, 3.33
 class ShopProduct
 {
-    public function __construct(
-        public $title,
-        public $producerFirstName = "",
-        public $producerMainName = "",
-        public $price = 0
-    ) {
+    public $title;
+    public $producerFirstName = "";
+    public $producerMainName = "";
+    public $price = 0;
 
+    public function __construct(
+        string $title,
+        string $producerFirstName,
+        string $producerMainName,
+        float $price = 0
+    ) {
+        $this->title = $title;
+        $this->producerFirstName = $producerFirstName;
+        $this->producerMainName = $producerMainName;
+        $this->price = $price;
     }
 
     public function getProducer()
